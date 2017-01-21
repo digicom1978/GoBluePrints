799c36d51409962a571869da1343ab661d4b1c75
Sd O <sdo@Sd-Os-MacBook.local>
2017-01-21 03:27:51 -0500

    add room.go

--- Chat/main.go
+++ Chat/main.go
@@ -1,32 +1,32 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1038\cocoasubrtf360
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww9000\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\ql\qnatural\pardirnatural
+package main
 
-\f0\fs24 \cf0 package main\
-\
-import (\
-	"log"\
-	"net/http"\
-)\
-\
-func main() \{\
-\
-	http.HandleFunc ("/", func (w http.ResponseWriter, r *http.Request) \{\
-		w.Write( []byte (`\
-			<html>\
-				<head>\
-					<title>Chat</title>\
-				</head>\
-				<body>\
-					Let's chat!\
-				</body>\
-			</html>\
-		`))\
-	\})\
-	// start the web server\
-	if err := http.ListenAndServe (":8080", nil); err := nil \{\
-		log.Fatal ("ListenAndServe:", err)\
-	\}\
-\}}
+import (
+	"log"
+	"net/http"
+	"text/template"
+	"path/filepath"
+	"sync"
+)
+
+// temple represents a single template
+type templateHandler struct {
+	once		sync.Once
+	filename	string
+	templ		*template.Template
+}
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must (template.ParseFiles(filepath.Join(".", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	// start the web server
+	if err := http.ListenAndServe (":8080", nil); err != nil {
+		log.Fatal ("ListenAndServe:", err)
+	}
+}
